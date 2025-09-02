dac8a42961c3736db4aa0ff1ad8d0aa71c309f6a
Hoodboy18 <castilloalejandro045@gmail.com>
2025-09-02 10:55:24 -0600

    Cambios 02/09/25

--- biblioteca/src/main/java/com/fca/biblioteca/domain/LibroDomain.java
+++ biblioteca/src/main/java/com/fca/biblioteca/domain/LibroDomain.java
@@ -1,4 +1,20 @@
 package com.fca.biblioteca.domain;
 
+import com.fca.biblioteca.data.Libro;
+import com.fca.biblioteca.data.LibroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class LibroDomain {
+    @Autowired
+    private LibroRepository libroRepository;
+
+    private List<Libro> buscarLibroPorTitulo(String titulo) {
+
+        return libroRepository.findAll();
+    }
 }
