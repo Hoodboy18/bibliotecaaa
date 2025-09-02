a3d96a1032e5f35802b42e9ff2c4e234418d819d
Hoodboy18 <castilloalejandro045@gmail.com>
2025-09-02 09:22:57 -0600

    Cambios 2

--- biblioteca/src/main/java/com/fca/biblioteca/data/Persona.java
+++ biblioteca/src/main/java/com/fca/biblioteca/data/Persona.java
@@ -1,6 +1,35 @@
 package com.fca.biblioteca.data;
 
-public class Persona {
-    private int idpersona;
-}
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "persona")
+public class Persona {
+
+    @Id
+    @Column(name = "idpersona")
+    private int idpersona;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "primerapellido")
+    private String primerapellido;
+
+    @Column(name = "segundoapellido")
+    private String segundoapellido;
+
+    public Persona() {}
+
+    public int getIdpersona() { return idpersona; }
+    public void setIdpersona(int idpersona) { this.idpersona = idpersona; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getPrimerapellido() { return primerapellido; }
+    public void setPrimerapellido(String primerapellido) { this.primerapellido = primerapellido; }
+
+    public String getSegundoapellido() { return segundoapellido; }
+    public void setSegundoapellido(String segundoapellido) { this.segundoapellido = segundoapellido; }
+}
